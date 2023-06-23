82a28263cbda065d2d807b473698585dd6b0f173
Stephen <skairu09@gmail.com>
2023-06-23 10:38:14 +0300

    Add navbar, shop and cart components and assets

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+        <div className='shopTitle'>
+            <h1>Kairu E-Shop</h1>
+        </div>
+        <div className='products'>
+            <h1></h1>
+        </div>
+    </div>
+  )
+}
